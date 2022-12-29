2ca0aefd48628efce70a3a58979ebea875f2fce1
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2022-12-29 13:39:40 -0600

    Operationes performed

--- school-grades.py
+++ school-grades.py
@@ -1,4 +1,14 @@
 '''
 Develop a program that asks for 3 school grades, and if the average is higher or equal to 5 show a message "Approved Student"
 '''
+grade1 = int(input("Introduce the first note: "))
+grade2 = int(input("Introduce the second note: "))
+grade3 = int(input("Introduce the third note: "))
 
+average = (grade1+grade2+grade3)/3
+round(average,2)
+
+if average>=5 :
+    print(f"Yor average is: {average} , Approved Student ")
+else:
+    print(f"Yor average is: {average} , Not Approved Student ")
